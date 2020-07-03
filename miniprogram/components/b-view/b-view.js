7aa98a428136cd168022afbc5e66249e3e645f49
5878794@qq.com <xf771026aa>
2020-07-03 16:06:49 +0800

    mdf  style  all add b-view test

--- miniprogram/components/b-view/b-view.js
+++ miniprogram/components/b-view/b-view.js
@@ -5,18 +5,15 @@ Component({
 	behaviors: [],
 	options: {
 		styleIsolation: 'apply-shared',      //外部样式会影响内部样式，组件样式不影响外部
-		virtualHost: true           //虚拟组件
+		// virtualHost:true           //虚拟组件
 	},
+	externalClasses: ['class'],
     properties: {
-	    class:{
-		    type:String,
-		    value:''
-	    },
-	    btt:{
-		    type:String,
-		    value:''
-	    },
 	    style:{
+	    	type:String,
+		    value:''
+	    },
+	    class:{
 	    	type:String,
 		    value:''
 	    }
